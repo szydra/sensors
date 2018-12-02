1979dbbded1d215eb9ee2c7f52d02c129dcb412c
szydra <szymon.draga@gmail.com>
2018-12-02 12:52:23 +0100

    Rename exception handler

--- src/main/java/io/relayr/sensors/controller/GlobalExceptionHandler.java
+++ src/main/java/io/relayr/sensors/controller/GlobalExceptionHandler.java
@@ -5,22 +5,23 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(NoSuchSensorException.class)
+    @ExceptionHandler(NoSuchSensorException.class)
     protected ResponseEntity<Object> handleNoSuchSensorException(NoSuchSensorException exception,
                                                                  WebRequest request) {
         return handleExceptionInternal(exception, exception.getMessage(),
                 new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(ConstraintViolationException.class)
+    @ExceptionHandler(ConstraintViolationException.class)
     protected ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException exception,
                                                                         WebRequest request) {
         return handleExceptionInternal(exception, "Validation errors: " + exception.getMessage(),
