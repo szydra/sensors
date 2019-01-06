e2990784b6eee3b2dce530a26a7e0515567c9af7
Szymon <szymon@dell.com>
2019-01-06 21:02:17 +0100

    Add a method for selecting sensors with locking

--- src/main/java/io/relayr/sensors/repository/SensorRepository.java
+++ src/main/java/io/relayr/sensors/repository/SensorRepository.java
@@ -1,8 +1,19 @@
 package io.relayr.sensors.repository;
 
-import io.relayr.sensors.model.Sensor;
+import java.util.Optional;
+
+import javax.persistence.LockModeType;
+
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import io.relayr.sensors.model.Sensor;
+
 public interface SensorRepository extends CrudRepository<Sensor, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Sensor s WHERE s.id = :id")
+    Optional<Sensor> findByIdForUpdate(Long id);
 
 }
